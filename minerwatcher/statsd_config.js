5581d7a00cb523072408099c5019d4b1cf9187c1
x46085 <x46085@gmail.com>
2014-06-01 16:12:55 -0700

    Opening flush timer to 10 minutes.

--- minerwatcher/statsd_config.js
+++ minerwatcher/statsd_config.js
@@ -1,6 +1,8 @@
 {
     graphitePort: 2003
-    , graphiteHost: "54.200.31.68"
+    , graphiteHost: "54.186.142.132"
     , port: 8125
     , backends: [ "./backends/graphite" ]
+    , flushInterval : 600000
+    , debug : true
 }
