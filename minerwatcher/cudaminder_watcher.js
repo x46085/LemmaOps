77cd3bc54c652eaacf6762a84ca527fa029f56e6
x46085 <x46085@gmail.com>
2014-06-14 09:36:50 -0700

    adding mac address

--- minerwatcher/cudaminder_watcher.js
+++ minerwatcher/cudaminder_watcher.js
@@ -5,6 +5,7 @@ var os     = require('os');
 var stats  = new StatsD();
 
 var gauge_prefix = 0;
+var mac = "00:00:00:00:00:00";
 
 function get_interface(){
 	var ifaces=os.networkInterfaces();
@@ -14,18 +15,21 @@ function get_interface(){
 		if (details.family=='IPv4') {
 		  //console.log(dev+(alias?':'+alias:''),details.address);
 		  if(details.address != 0 && details.address != "127.0.0.1"){
-		  gauge_prefix = details.address;
+		  	gauge_prefix = details.address;
+			mac = ifaces[dev][1].address;
+			//console.log(ifaces[dev][1].address);
 		  }
 		  ++alias;
 		} 
 	  });
 	}
+	
 }
 
 function start_mining(){
 	gauge_prefix_array = gauge_prefix.split(".");
 	//console.log(guage_prefix_array[3]);
-	gauge_prefix = gauge_prefix_array[3];
+	gauge_prefix = gauge_prefix_array[2]+"_"+gauge_prefix_array[3];
 
 	var current_temp = 0;
 	var cudatemp = execute('nvidia-settings -q gpucoretemp -t',
@@ -38,6 +42,9 @@ function start_mining(){
 	var regexSplit = /.+, /;
 	var regexMatchHash = /([0-9]+[ ][k][h])/;
 
+	var id = Math.floor(Math.random()*30);
+	// blake
+	/*
 	var args = ["--algo=blake",
 		    "--url=stratum+tcp://la1.blakecoin.com:3334", 
 		    "--user=BTG-LTG-miningtest."+gauge_prefix,
@@ -48,14 +55,18 @@ function start_mining(){
 		    "--interactive=0",
 		    "--texture-cache=0",
 		    "-b=2048",
-		    "--single-memory=0"];
+		    "--single-memory=0"];*/
 
-	//var cudaminer = spawn('/home/lemma/CudaMiner/cudaminer', args);
-	//console.log(cudaminer+" "+args);
+	// x11
+	var args = ["--algo=x11",
+		    "--url=stratum+tcp://192.168.2.85:3333", 
+		    "--user=x46085."+id,
+		    "--pass=x"];
 
-	var cudaminer = spawn('/home/lemma/Desktop/start_miner.sh', []);
+	var cudaminer = spawn('/home/lemma/CudaMiner/cudaminer', args);
+	console.log(cudaminer+" "+args);
 
-	
+	//var cudaminer = spawn('/home/lemma/Desktop/start_miner.sh', []);
 
 	cudaminer.stderr.setEncoding('utf8');
 	cudaminer.stderr.on('data', function (data) {
@@ -72,8 +83,8 @@ function start_mining(){
 				//console.log(hashRate[0]);
 				cudatemp = execute('nvidia-settings -q gpucoretemp -t',
 		  			function (error, stdout, stderr) { current_temp = parseInt(stdout); });
-				stats.gauge(gauge_prefix+".HashRate", parseInt(hashRate[0]));
-				stats.gauge(gauge_prefix+".Temp", current_temp);
+				stats.gauge(mac+".HashRate", parseInt(hashRate[0]));
+				stats.gauge(mac+".Temp", current_temp);
 			    }
 		        } catch(err) {
 		            //console.log("err:", err);
@@ -93,6 +104,7 @@ function start_mining(){
 
 function connect_delay(){
 	get_interface();
+	
 	if(gauge_prefix == 0){
 		console.log('network inferface not found, waiting 10 seonds');
 		setTimeout(connect_delay, 10000);
@@ -103,25 +115,4 @@ function connect_delay(){
 
 connect_delay();
 
-			    /* Old Miner Stats, requires modifying cudaminer:
-		            var jdata = JSON.parse(datalist[i]);
-					console.log(datalist[i]);		
-					if(jdata["gfxCard"]){  
-						//Format: "{gpuNum:%d , gfxCard:%s, hashes:%lu, hashRate:%s }" 
-						cudatemp = execute('nvidia-settings -q gpucoretemp -t',
-		  		    		function (error, stdout, stderr) { current_temp = parseInt(stdout); });
-						jdata.gfxCard.replace(" ", "_");
-						stats.gauge(gauge_prefix+".GPU"+jdata.gpuNum+".HashRate", jdata.hashRate);
-						stats.gauge(gauge_prefix+".GPU"+jdata.gpuNum+".Temp", current_temp);
-					} else if (jdata["accepted"]){ 
-						//Format: "{accepted:%lu, rejected:%lu, percent:%.2f%%, khashs:%s, success: %s}"
-						if(Boolean(jdata.success)){
-							stats.increment(gauge_prefix+".AcceptCount");
-						} else {
-							stats.increment(gauge_prefix+".RejectCount");
-						}
-						stats.gauge(gauge_prefix+".TotalKHs", jdata.khashs);
-						stats.gauge(gauge_prefix+".AcceptPercent", jdata.percent);
-					}
-			   */
 
